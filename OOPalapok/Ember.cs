00d596c1be9de5edbe9318226cafd11d498d7beb
Petrik Diak <diak@petrik.hu>
2025-02-04 08:46:54 +0100

    Add project files.

---
+++ OOPalapok/Ember.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+	public class Ember
+	{
+		private double testSuly;
+		private int testMagassag;
+
+		public Ember(double testSuly, int testMagassag)
+		{
+			this.testSuly = testSuly;
+			this.testMagassag = testMagassag;
+		}
+
+		public override string ToString()
+		{
+			return $"Testsúly: {testSuly} kg, Magasság: {testMagassag} cm";
+		}
+
+		public double TestTomegIndex()
+		{
+			double magassagMeterben = testMagassag / 100.0;
+			double tti = testSuly / (magassagMeterben * magassagMeterben);
+			return Math.Round(tti, 2);
+		}
+
+		public bool NormalTTI()
+		{
+			double tti = TestTomegIndex();
+			return tti >= 18.5 && tti < 25;
+		}
+	}
+}
---
+++ OOPalapok/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPalapok
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Ember ember1 = new Ember(70.5, 175);
+			Console.WriteLine(ember1.ToString());
+			Console.WriteLine($"Testtömegindex: {ember1.TestTomegIndex()}");
+			Console.WriteLine($"Normális testtömegindex: {ember1.NormalTTI()}");
+            Console.WriteLine();
+
+            Ember ember2 = new Ember(50.0, 180);
+			Console.WriteLine(ember2.ToString());
+			Console.WriteLine($"Testtömegindex: {ember2.TestTomegIndex()}");
+			Console.WriteLine($"Normális testtömegindex: {ember2.NormalTTI()}");
+            Console.WriteLine();
+
+            Ember ember3 = new Ember(90.0, 160);
+			Console.WriteLine(ember3.ToString());
+			Console.WriteLine($"Testtömegindex: {ember3.TestTomegIndex()}");
+			Console.WriteLine($"Normális testtömegindex: {ember3.NormalTTI()}");
+            Console.WriteLine();
+        }
+	}
+}
