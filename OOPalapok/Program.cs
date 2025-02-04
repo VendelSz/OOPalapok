88115e5bba7b5aa67519cdb86b81c01e5e3fbc09
Petrik Diak <diak@petrik.hu>
2025-02-04 09:18:42 +0100

    wasd

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -4,23 +4,12 @@
 	{
 		static void Main(string[] args)
 		{
-			Ember ember1 = new Ember(70.5, 175);
-			Console.WriteLine(ember1.ToString());
-			Console.WriteLine($"Testtömegindex: {ember1.TestTomegIndex()}");
-			Console.WriteLine($"Normális testtömegindex: {ember1.NormalTTI()}");
-            Console.WriteLine();
+			Emberek emberek = new Emberek(5);
 
-            Ember ember2 = new Ember(50.0, 180);
-			Console.WriteLine(ember2.ToString());
-			Console.WriteLine($"Testtömegindex: {ember2.TestTomegIndex()}");
-			Console.WriteLine($"Normális testtömegindex: {ember2.NormalTTI()}");
-            Console.WriteLine();
+			Console.WriteLine(emberek.ToString());
 
-            Ember ember3 = new Ember(90.0, 160);
-			Console.WriteLine(ember3.ToString());
-			Console.WriteLine($"Testtömegindex: {ember3.TestTomegIndex()}");
-			Console.WriteLine($"Normális testtömegindex: {ember3.NormalTTI()}");
-            Console.WriteLine();
-        }
+			Console.WriteLine($"Átlagos testsúly: {emberek.AtlagTestsuly():F1} kg");
+			Console.WriteLine($"Átlagos testmagasság: {emberek.AtlagTestmagassag():F1} cm");
+		}
 	}
 }
